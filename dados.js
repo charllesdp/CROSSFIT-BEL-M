8540c23e15f11344db76ebcd167529755af23099
charlles felipe <125304834+charllesdp@users.noreply.github.com>
2024-09-07 20:30:02 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados = [
+    {
+        titulo: "Black Cross 392",
+        descricao: "Desafie seus limites e transforme suor em conquistas. Mais que um espaço de treino, somos uma comunidade unida em busca de excelência física e mental.",
+        link: "https://www.instagram.com/blackcross392/",
+        tags: "marambaia",
+        
+    },
+    {
+        titulo: "Box Cross A&P",
+        descricao: "A superação é um processo inspirador, no qual enfrentamos desafios e obstáculos para alcançar nossos objetivos. É uma jornada de crescimento pessoal e aprendizado, onde descobrimos nossa força interior e capacidade de adaptação. A superação não é apenas sobre vencer, mas também sobre aprender com as dificuldades e se tornar uma versão melhor de si mesmo.",
+        link: "https://www.instagram.com/boxcrossaep/",
+        tags: "marambaia"
+    },
+    {
+        titulo: "Box Cross Do ZB",
+        descricao: "A nossa missão é cuidar da sua Saúde Aqui cada passo que você dá em direção a um estilo de vida mais saudável e com ótimos resultados nos enche de alegria! Aqui você não é apenas mais um aluno. Você é parte de uma família que se apoia, se motiva e cresce junto.",
+        link: "https://www.instagram.com/boxcrosszb/",
+        tags: "marambaia"
+    },
+    
+];
